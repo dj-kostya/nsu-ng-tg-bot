6adeda53aa566de43571cc18427588b03d842ced
djkostya <djkoo2001@gmail.com>
2019-09-06 00:25:52 +0700

    HEroku

--- app_body/__init__.py
+++ app_body/__init__.py
@@ -4,9 +4,9 @@ from flask import Flask, request
 from app_body.Settings import Contants
 from .BD import db
 
-from .routes import app
+
 from .tg import bot, init
+from .routes import app
 debug = os.environ.get('DATABASE_URL') is None
 db.set_session()
 init()
-
