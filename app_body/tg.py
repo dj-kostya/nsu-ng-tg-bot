a682717bc3898ce7fb2c7b02651291d28575571a
djkostya <djkoo2001@gmail.com>
2019-09-05 18:25:28 +0700

    HEroku

--- app_body/tg.py
+++ app_body/tg.py
@@ -5,8 +5,23 @@ import telebot
 bot = telebot.TeleBot(Contants.TG_BOT_TOKEN)
 
 
+def process_group_step(message):
+    try:
+        msg = bot.reply_to(message, 'How old are you?')
+        # bot.register_next_step_handler(msg, process_group_step)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    print(message)
-
-
+    tg_id = message['from_user']['id']
+    row = db.Users.query.filter_by(tg_id=tg_id).first()
+    if not row:
+        rows = db.Groups.all()
+        print(rows)
+        msg = bot.reply_to(message, """\
+        Привет, кажется ты еще не зарегистрирован
+        What's your name?
+        """)
+        bot.register_next_step_handler(msg, process_group_step)
