37f6cb639ca597fc73e6ddf370540559834e2acb
djkostya <djkoo2001@gmail.com>
2019-09-05 17:54:31 +0700

    HEroku

--- run_server.py
+++ run_server.py
@@ -1,6 +1,6 @@
 import argparse
 
-from app_body import app, Contants, debug, bot
+from app_body import app, Contants, debug
 
 if __name__ == '__main__':
     print('start')
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     port = args.port
     print(debug)
     if not debug:
+        from app_body import bot
         bot.remove_webhook()
         bot.set_webhook(url=Contants.WEBHOOK_URL+Contants.WEBHOOK_PATH)
     app.run(host=Contants.FLASK_HOST, port=port, debug=debug)
