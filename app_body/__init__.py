2caa5696851c49f3d0b04840016d35f233063734
djkostya <djkoo2001@gmail.com>
2019-09-06 00:23:17 +0700

    HEroku

--- app_body/__init__.py
+++ app_body/__init__.py
@@ -4,9 +4,9 @@ from flask import Flask, request
 from app_body.Settings import Contants
 from .BD import db
 
-
-from .tg import bot
 from .routes import app
+
 debug = os.environ.get('DATABASE_URL') is None
 db.set_session()
 admin_group = db.Groups.query.filter_by(name='Преподаватели').first()
+from .tg import bot
