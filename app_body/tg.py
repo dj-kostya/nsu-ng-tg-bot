23480e013d4abe746c4a61f0763df062f2eb169c
djkostya <djkoo2001@gmail.com>
2019-09-05 19:09:28 +0700

    HEroku

--- app_body/tg.py
+++ app_body/tg.py
@@ -6,18 +6,21 @@ bot = telebot.TeleBot(Contants.TG_BOT_TOKEN)
 
 
 def process_group_step(message):
-    try:
-        print(message)
-        rows = db.Groups.all()
-        msg = bot.reply_to(message, "\
-                Привет, кажется ты еще не зарегистрирован\nВыбери свою группу:" + '\n'.join(
-            [
-                '{id} - > {name}'.format(id=group.id, name=group.name)
-                for group in rows
-            ]
-        ))
-    except Exception as e:
-        bot.reply_to(message, 'oooops')
+    chat_id = message.chat.id
+    text = message.text
+    if not text.isdigit():
+        msg = bot.send_message(chat_id, 'Группа должна быть числом')
+        bot.register_next_step_handler(msg, process_group_step)
+        return
+    if not db.Groups.find(int(text)):
+        msg = bot.send_message(chat_id, 'Не верная группа')
+        bot.register_next_step_handler(msg, process_group_step)
+        return
+    user = db.Users.create(tg_username=message.from_user.username, tg_id=message.from_user.id, id_role=int(text),
+                           id_group=1)
+    db.commit()
+    msg = bot.send_message(chat_id, 'Я все созхранил')
+    bot.register_next_step_handler(msg, start_command)
 
 
 @bot.message_handler(commands=['start'])
@@ -28,9 +31,9 @@ def start_command(message):
     print(tg_id)
     row = db.Users.query.filter_by(tg_id=tg_id).first()
     if not row:
-        rows = db.Groups.all()
-        msg = bot.reply_to(message, "\
-                        Привет, кажется ты еще не зарегистрирован\nВыбери свою группу:" + '\n'.join(
+        rows = db.Groups.create()
+        msg = bot.send_message(tg_id, "\
+                        Привет, кажется ты еще не зарегистрирован\nВыбери свою группу:\n" + '\n'.join(
             [
                 '{id} - > {name}'.format(id=group.id, name=group.name)
                 for group in rows
