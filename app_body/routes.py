22fd4449f8704d96fac37a1ed05e316bb1fc7553
djkostya <djkoo2001@gmail.com>
2019-09-07 22:38:50 +0700

    HEroku

--- app_body/routes.py
+++ app_body/routes.py
@@ -15,6 +15,7 @@ def index():
 def webhook():
     if request.headers.get('content-type') == 'application/json':
         json_string = request.get_data().decode('utf-8')
+        print(json_string)
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
         return ''
