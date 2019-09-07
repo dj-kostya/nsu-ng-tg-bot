5278f8aeb96fc3e5e48191ac2580269b9c17a9a9
djkostya <djkoo2001@gmail.com>
2019-09-07 22:53:11 +0700

    HEroku

--- app_body/routes.py
+++ app_body/routes.py
@@ -11,11 +11,17 @@ def index():
     return "!", 200
 
 
+messages = {}
+
+
 @app.route(Contants.WEBHOOK_PATH, methods=['POST'])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
         json_string = request.get_data().decode('utf-8')
-        print(json_string)
+        chat = json_string['message']['chat']
+        chat_id = json_string['message']['chat']['id']
+        date = json_string['message']['date']
+        print(chat_id, date)
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
         return ''
